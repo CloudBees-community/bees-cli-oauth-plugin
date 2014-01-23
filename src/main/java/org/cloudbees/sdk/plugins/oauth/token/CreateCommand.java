2d2ec9587d5898d8e3e54d12e395e5bb2186ab77
Vivek Pandey <vivek.pandey@gmail.com>
2014-01-23 11:42:23 -0800

    Generate token using client id and secret.

--- src/main/java/org/cloudbees/sdk/plugins/oauth/token/CreateCommand.java
+++ src/main/java/org/cloudbees/sdk/plugins/oauth/token/CreateCommand.java
@@ -8,7 +8,6 @@ import com.cloudbees.sdk.cli.BeesCommand;
 import com.cloudbees.sdk.cli.CLICommand;
 import org.kohsuke.args4j.Option;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +15,7 @@ import java.util.List;
  * Generates a new token.
  *
  * @author Kohsuke Kawaguchi
+ * @author Vivek Pandey
  */
 @BeesCommand(group="OAuth",description="Create a new token")
 @CLICommand("oauth:token:create")
@@ -32,14 +32,30 @@ public class CreateCommand extends AbstractTokenCommand {
     @Option(name="--account",usage="Account in which the app gets registered.")
     public String account;
 
+    @Option(name="-generate-refresh-token",usage="Generate refresh_token along with access_token.")
+    public boolean generateRefreshToken;
+
+    @Option(name="-clientId",usage="Generate token with clientId")
+    public String clientId;
+
+    @Option(name="-clientSecret",usage="Generate token with clientSecret")
+    public String clientSecret;
+
     @Override
     public int main() throws Exception {
-        String account = promptAccount(this.account, "Account that the generated token will grant access to.");
-        TokenRequest req = new TokenRequest(note,noteUrl,null,account,scopes.toArray(new String[scopes.size()]));
+        TokenRequest req = new TokenRequest().withScopes(scopes.toArray(new String[scopes.size()])).withAccountName(account);
+        req.withGenerateRequestToken(generateRefreshToken);
 
-        OauthClient oac = createClient();
-        OauthToken token = oac.createToken(req);
-        if (token==null) {
+        OauthClient beesClient = createClient();
+        OauthToken token;
+        if (clientId != null && clientSecret!=null) {
+            token = new BeesClient(factory.getApiUrl(),clientId,clientSecret).getOauthClient().createOAuthClientToken(req);
+        }else{
+            String account = promptAccount(this.account, "Account that the generated token will grant access to.");
+            req.withNote(note, noteUrl).withAccountName(account);
+            token = beesClient.createToken(req);
+        }
+        if (token == null) {
             System.err.println("Failed to create a token");
             return 1;
         }
